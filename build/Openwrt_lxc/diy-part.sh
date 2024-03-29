d1ea7c74bd80d1b9af9f5a71249637f2e309d414
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-03-29 22:56:43 +0800

    Update diy-part.sh

--- build/Openwrt_lxc/diy-part.sh
+++ build/Openwrt_lxc/diy-part.sh
@@ -71,12 +71,12 @@ fi
 # x86机型,默认内核6.1，修改内核为6.1
 #export NEW_KERNEL_PATCHVER="6.6"
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/small-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git sirpdboy  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
 sed -i '4i src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
 
-
+rm -rf feeds/sirpdboy/
 # 禁用ipv6前缀
 #sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 ##########################################添加插件###################################################
@@ -84,6 +84,9 @@ pushd ${HOME_PATH}/package > /dev/null
 
 #echo "添加插件 luci-app-passwall"
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+git clone --depth=1 https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
+
 
 #echo "添加插件 luci-app-ssr-plus"
 #git clone --depth=1 https://github.com/fw876/helloworld
