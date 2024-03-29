7b9872b9b746115840b8e92e6bd900c0aa5d5292
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-03-30 07:41:19 +0800

    Update diy-part.sh

--- build/Openwrt_lxc/diy-part.sh
+++ build/Openwrt_lxc/diy-part.sh
@@ -71,12 +71,11 @@ fi
 # x86机型,默认内核6.1，修改内核为6.1
 #export NEW_KERNEL_PATCHVER="6.6"
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/small-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git sirpdboy  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-sed -i '4i src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
-sed -i '5i src-git wolplus https://github.com/animegasan/luci-app-wolplus' feeds.conf.default
-rm -rf feeds/sirpdboy/
+sed -i '4i src-git openwrt https://git.openwrt.org/feed/packages.git' feeds.conf.default
+#sed -i '5i src-git wolplus https://github.com/animegasan/luci-app-wolplus' feeds.conf.default
 # 禁用ipv6前缀
 #sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 ##########################################添加插件###################################################
