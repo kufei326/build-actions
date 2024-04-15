d1b16866fb9846256e0ef8219671e1783adbf69c
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-04-15 20:29:44 +0800

    Update diy-part.sh

--- build/Openwrt_lxc/diy-part.sh
+++ build/Openwrt_lxc/diy-part.sh
@@ -71,19 +71,12 @@ fi
 # x86机型,默认内核6.1，修改内核为6.1
 #export NEW_KERNEL_PATCHVER="6.6"
 
-cat <<EOF > feeds.conf.default
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-src-git sirpdboy https://github.com/kufei326/sirpdboy-package
-src-git packages https://github.com/openwrt/packages.git
-src-git luci https://github.com/openwrt/luci.git
-src-git routing https://github.com/openwrt/routing.git
-src-git telephony https://github.com/openwrt/telephony.git
-EOF
-
-#./scripts/feeds update routing && ./scripts/feeds install -a -p routing
-#./scripts/feeds update luci && ./scripts/feeds install -a -p luci
-#./scripts/feeds update telephony && ./scripts/feeds install -a -p telephony
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git sirpdboy  https://github.com/kufei326/sirpdboy-package' feeds.conf.default
+#sed -i '4i src-git packages https://github.com/openwrt/packages.git' feeds.conf.default
+#sed -i '5i src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
+#sed -i '6i src-git routing https://github.com/openwrt/routing.git' feeds.conf.default
 # 禁用ipv6前缀
 #sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 ##########################################添加插件###################################################
