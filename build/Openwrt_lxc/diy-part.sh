7bb9a9a8d63f02b8fe609c98fbf6980b1ab952c9
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-05-29 07:56:40 +0800

    Update diy-part.sh

--- build/Openwrt_lxc/diy-part.sh
+++ build/Openwrt_lxc/diy-part.sh
@@ -71,9 +71,9 @@ fi
 # x86机型,默认内核6.1，修改内核为6.1
 #export NEW_KERNEL_PATCHVER="6.6"
 
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '3i src-git sirpdboy  https://github.com/kufei326/sirpdboy-package' feeds.conf.default
+sed -i '3i src-git sirpdboy  https://github.com/kufei326/sirpdboy-package' feeds.conf.default
 #sed -i '4i src-git packages https://github.com/openwrt/packages.git' feeds.conf.default
 #sed -i '5i src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
 #sed -i '6i src-git routing https://github.com/openwrt/routing.git' feeds.conf.default
